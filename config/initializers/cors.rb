52e7fb905cd6d7b39fae49a83e29b2a12facd574
yuma-matsui <oshino.iiyo@gmail.com>
2022-12-03 11:16:53 +0900

    本番環境用のCORS設定を追加した

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['FRONT_DOMAIN'] || ''
+    origins ENV['FRONT_DOMAIN'] || Rails.application.credentials.production.front[:origin]
 
     resource "*",
       headers: :any,
