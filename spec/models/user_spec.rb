662d0cd7a2fb13bfcb9f734736afe7d875abc82a
yuma-matsui <oshino.iiyo@gmail.com>
2023-02-18 13:58:11 +0900

    CIを通すための簡易なモデルスペックを書いた

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.build(:user) }
+
+  describe 'validation' do
+    describe 'name' do
+      example 'valid' do
+        expect(user).to be_valid
+      end
+
+      example 'invalid' do
+        user.name = ''
+        expect(user).to be_invalid
+      end
+    end
+
+    describe 'uid' do
+      example 'valid' do
+        expect(user).to be_valid
+      end
+
+      example 'invalid' do
+        user.uid = ''
+        expect(user).to be_invalid
+      end
+    end
+  end
 end
