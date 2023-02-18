472f60953c5f8189c220b009b0261fc490e26119
yuma-matsui <oshino.iiyo@gmail.com>
2023-02-18 20:10:40 +0900

    Userモデルにuidの一意制約を追加した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :uid, presence: true
+  validates :uid, presence: true, uniqueness: true
   validates :name, presence: true
 end
