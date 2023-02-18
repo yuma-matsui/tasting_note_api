ae02f7815e47c801a3144e9c39a7d30579f9be0b
yuma-matsui <oshino.iiyo@gmail.com>
2023-02-18 21:20:48 +0900

    Userテーブルからnameカラムを削除した

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_18_121330) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_18_121759) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "uid", null: false
-    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", null: false
