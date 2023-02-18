04f0217bcf350c01ae50998f44458e8662e5eb02
yuma-matsui <oshino.iiyo@gmail.com>
2023-02-18 11:34:26 +0900

    フロントエンドからのリクエストに対してユーザーを作成してcurrent_userとしてセットする処理を加えた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+
+  before_action :authenticate
+  attr_reader :current_user
+
+  private
+
+  def authenticate
+    authenticate_with_http_token { |token| find_current_user(FirebaseAuth::Token.payload(token)) }
+  end
+
+  def find_current_user(payload)
+    @current_user = User.find_or_create_by!(uid: payload['sub'], name: payload['name'])
+  end
 end
