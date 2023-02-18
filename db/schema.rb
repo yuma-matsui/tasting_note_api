9a45d73a4031b2efb0850c0c225d3df0c5eb0e01
yuma-matsui <oshino.iiyo@gmail.com>
2023-02-18 21:19:18 +0900

    Uesrテーブルにemailカラムを追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_18_105627) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_18_121330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_18_105627) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", null: false
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
