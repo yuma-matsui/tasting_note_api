33b97af25fed5be4b8bcd9f0f940794e5ecb6930
yuma-matsui <oshino.iiyo@gmail.com>
2023-02-18 22:08:46 +0900

    Usersテーブルのカラムを変更したのでテストも書き直した

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 
 FactoryBot.define do
   factory :user do
-    uid  { 'testUid' }
-    name { 'taro' }
+    sequence(:uid)   { |n| "testUid-#{n}" }
+    sequence(:email) { |n| "test#{n}@example.com" }
   end
 end
