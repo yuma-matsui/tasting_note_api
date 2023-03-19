fdd9b9361a1d5c82709d5de2f184adcd5692cbcf
yuma-matsui <oshino.iiyo@gmail.com>
2023-03-19 16:49:32 +0900

    ワインの新規作成用のルーティングを追加した

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :users, only: :destroy
       resources :sessions, only: :index
       resources :tasting_sheets, only: %i(index create destroy show update)
+      resources :wines, only: :create
     end
   end
 end
