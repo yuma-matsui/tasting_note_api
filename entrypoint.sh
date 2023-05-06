2cdf01d26f23e34e5d40c4e458e9e7ef3ef27ffb
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-06 19:07:55 +0900

    本番環境のDBリセットが済んだので不要になった処理を削除した

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@ set -e
 
 rm -f ./tmp/pids/server.pid
 
-bundle exec rails db:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 bundle exec rails db:migrate RAILS_ENV=production
 
 exec "$@"
