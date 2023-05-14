adc6ac50508c591a7e46e5b8cf1ff2b01faa673a
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-14 10:00:57 +0900

    Appearanceのdependent destroyに関するテストを修正した

--- spec/factories/appearances.rb
+++ spec/factories/appearances.rb
@@ -7,5 +7,13 @@ FactoryBot.define do
     intensity   { '無色に近い' }
     consistency { 'さらっとした' }
     tasting_sheet
+
+    trait :with_appearance_color do
+      after(:create) { |appearance| create_list(:appearance_color, 1, appearance: appearance) }
+    end
+
+    trait :with_appearance_impression do
+      after(:create) { |appearance| create_list(:appearance_impression, 1, appearance: appearance) }
+    end
   end
 end
--- spec/models/appearance_spec.rb
+++ spec/models/appearance_spec.rb
@@ -3,19 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe Appearance, type: :model do
-  let(:appearance) { FactoryBot.build(:appearance) }
-
   describe 'validation' do
+    appearance = FactoryBot.build(:appearance)
     it 'is valid with clarity, brightness, intensity, consistency' do
       expect(appearance).to be_valid
     end
   end
 
   describe 'dependent destroy' do
-    it 'is destroyed when tasting sheet is destroyed' do
-      appearance.save
-      expect { appearance.tasting_sheet.destroy }.to \
-        change(described_class, :count).by(-1)
+    it 'destroys appearance_color when appearance is destroyed' do
+      appearance = FactoryBot.create(:appearance, :with_appearance_color)
+      expect { appearance.destroy }.to \
+        change(AppearanceColor, :count).by(-1)
+    end
+
+    it 'destroys appearance_impression when appearance is destroyed' do
+      appearance = FactoryBot.create(:appearance, :with_appearance_impression)
+      expect { appearance.destroy }.to \
+        change(AppearanceImpression, :count).by(-1)
     end
   end
 end
