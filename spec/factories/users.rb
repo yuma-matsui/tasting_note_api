9a442e09f4ced3655c1656f4a474d772de3ec08e
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-14 10:32:27 +0900

    Userのmodel specにdependent destroyに関するテストを追加した

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
   factory :user do
     sequence(:uid)   { |n| "test-uid-#{n}" }
     sequence(:email) { |n| "test-mail-#{n}@example.com" }
+
+    trait :with_tasting_sheet do
+      after(:create) { |user| create_list(:tasting_sheet, 1, user: user) }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { FactoryBot.build(:user) }
-  let(:other_user) { FactoryBot.build(:user) }
+  describe 'validation' do
+    let(:user) { FactoryBot.build(:user) }
+    let(:other_user) { FactoryBot.build(:user) }
 
-  it 'is valid with uid and email' do
-    expect(user).to be_valid
+    it 'is valid with uid and email' do
+      expect(user).to be_valid
+    end
+
+    it 'is invalid without unique email ' do
+      user.save
+      other_user.email = user.email
+      expect(other_user).to be_invalid
+    end
   end
 
-  it 'is invalid without unique email ' do
-    user.save
-    other_user.email = user.email
-    expect(other_user).to be_invalid
+  describe 'dependent destroy' do
+    it 'destroys tasting_sheet when user is destroyed' do
+      user = FactoryBot.create(:user, :with_tasting_sheet)
+      expect { user.destroy }.to \
+        change(TastingSheet, :count).by(-1)
+    end
   end
 end
