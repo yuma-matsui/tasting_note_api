221db05df2bac15c5474e136ca41d11c2e019f50
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-14 10:42:48 +0900

    AppearaceColor AppearanceImpressionのmodel specを修正した

--- spec/models/appearance_color_spec.rb
+++ spec/models/appearance_color_spec.rb
@@ -5,9 +5,7 @@ require 'rails_helper'
 RSpec.describe AppearanceColor, type: :model do
   let(:appearance_color) { FactoryBot.build(:appearance_color) }
 
-  describe 'validation' do
-    it 'is valid with color' do
-      expect(appearance_color).to be_valid
-    end
+  it 'is valid with color' do
+    expect(appearance_color).to be_valid
   end
 end
--- spec/models/appearance_impression_spec.rb
+++ spec/models/appearance_impression_spec.rb
@@ -5,9 +5,7 @@ require 'rails_helper'
 RSpec.describe AppearanceImpression, type: :model do
   let(:appearance_impression) { FactoryBot.build(:appearance_impression) }
 
-  describe 'validation' do
-    it 'is valid with color' do
-      expect(appearance_impression).to be_valid
-    end
+  it 'is valid with impression' do
+    expect(appearance_impression).to be_valid
   end
 end
