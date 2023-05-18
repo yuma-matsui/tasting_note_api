30fd2cfa58b58aacb855b2234eafe3b54050d8b8
yuma-matsui <oshino.iiyo@gmail.com>
2023-05-18 22:52:51 +0900

    Api::V1::HealthCheckControllerのrequest specのファイル名を修正した

--- spec/requests/api/v1/health_check_spec.rb
+++ spec/requests/api/v1/health_check_spec.rb
@@ -2,8 +2,8 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Api::V1::HealthChecks', type: :request do
-  describe 'GET /api/v1/health_checks' do
+RSpec.describe 'Api::V1::HealthCheck', type: :request do
+  describe 'GET /api/v1/health_check' do
     it 'returns ok status' do
       get api_v1_health_check_index_path
       expect(response).to have_http_status :ok
